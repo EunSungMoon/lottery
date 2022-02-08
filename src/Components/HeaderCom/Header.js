13bac32ff604baf71776639050848215913abfe0
Eunsung Moon <eunsung9647@gmail.com>
2022-02-08 22:55:30 +0900

    지난 회차 로또 당첨 번호 가져오기

--- src/Components/HeaderCom/Header.js
+++ src/Components/HeaderCom/Header.js
@@ -2,7 +2,7 @@
 export default function Header() {
   return (
     <header>
-      <h1 className="h1">로또 번호 생성기</h1>
+      <h1 className="h1">Lottery CLUB</h1>
     </header>
     )
 }
--- src/Components/RecentNumberCom/RecentNumber.js
+++ src/Components/RecentNumberCom/RecentNumber.js
@@ -1,38 +1,46 @@
 import axios from 'axios';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function RecentNumber() {
   const [lottery, setLottery] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const handleButton = () => {
-    console.log('test')
-    loadAxios()
-  }
-
   const loadAxios = async () => {
     try {
       setError(null);
       setLottery(null);
       setLoading(true);
-      let loadData = await axios.get(`https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=903`, {
+      const loadData = await axios.get('http://15.164.62.156:8000/api/lottery/', {
         headers: {
           'Content-Type': 'application/json'
         }
       })
-      setLottery(loadData)
+      setLottery(loadData.data)
       console.log(loadData)
     }
     catch (error) {
+      setError(error)
       console.log(error)
     }
+    setLoading(false)
   }
+
+  useEffect(() => {
+    loadAxios()
+    return lottery;
+  }, []);
+  if (loading) return <div>로딩중...</div>
+  if (error) return <div>에러가 발생했습니다.</div>
+  if (!lottery) return null;
 
   return (
     <div>
-      <span>지난주 로또 번호 확인하기</span>
-      <button onClick={handleButton}>click!</button>
+      <span>{lottery.number}회 로또 번호 확인하기</span>
+      <span>{lottery.date}</span>
+      <div>
+        {lottery.n1}, {lottery.n2}, {lottery.n3}, {lottery.n4}, {lottery.n5}, {lottery.n6}, {lottery.n7}
+      </div>
     </div>
   )
 }
