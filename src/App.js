73a88c8477e04d847da7972722cbb832922241a3
Eunsung Moon <eunsung9647@gmail.com>
2022-06-25 22:32:00 +0900

    error page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import './style/App.scss';
 import Header from './Components/HeaderCom/Header';
 import MainPage from './Pages/MainPage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import EmptyPage from './Pages/EmptyPage';
 
 function App() {
   return (
-    <div className='App'>
-      <div className='container-fluid'>
-        <Header />
-        <MainPage />
+    <BrowserRouter>
+      <div className='App'>
+        <div className='container-fluid'>
+          <Routes>
+            <Route exact path='/' element={
+              <><Header /><MainPage /></>
+            }>
+
+            </Route>
+            <Route path='*' element={<EmptyPage />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
